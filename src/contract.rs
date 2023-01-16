fdccabadaa38a2d7d9394fb873e3c6484c60d269
ADVK <advaiyakarnik@gmail.com>
2023-01-16 18:32:49 +0530

    impl

--- src/contract.rs
+++ src/contract.rs
@@ -5,3 +5,19 @@ use near_sdk::serde::Serialize;
 use near_sdk::{env, near_bindgen, AccountId, Balance};
 
 const POINT_ONE: Balance = 100_000_000_000_000_000_000_000;
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct PostedMessage {
+    pub premium: bool,
+    pub sender: AccountId,
+    pub text: String,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+struct questBook {
+    message: Vector<PostedMessage>,
+}
+
+impl Default for questBook {}
